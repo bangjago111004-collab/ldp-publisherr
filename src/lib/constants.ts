dafd9e9a41ba13dd96156d63e3896ea47df4cfc2
bangjago111004-collab <bangjago111004@gmail.com>
2026-01-20 11:02:44 +0700

    initial commit

---
+++ src/lib/constants.ts
@@ -0,0 +1,64 @@
+export const COMPANY_INFO = {
+  name: "LDP Publisher",
+  fullName: "PT Lentera Duta Persada",
+  tagline: "Mewujudkan Karya, Menerangi Dunia",
+  description: "Penerbit profesional yang berkomitmen menghadirkan karya-karya berkualitas tinggi untuk memperkaya khazanah literasi Indonesia.",
+  mainWebsite: "https://ldpgroup.co.id",
+  
+  contact: {
+    email: "publisher@ldpgroup.co.id",
+    phone: "+62 831 5219 5459",
+    whatsapp: "6283152195459",
+    address: "5H3J+GW Cigugur Girang, Kabupaten Bandung Barat, Jawa Barat",
+  },
+  
+  social: {
+    instagram: "https://www.instagram.com/ldppublisher?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
+    linkedin: "https://linkedin.com/company/ldp-publisher",
+    whatsapp: "https://wa.me/6283152195459",
+  },
+  
+  location: {
+    googleMapsUrl: "https://www.google.com/maps/search/?api=1&query=5H3J%2BGW+Cigugur+Girang%2C+Kabupaten+Bandung+Barat%2C+Jawa+Barat",
+    embedUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3961.2!2d107.4889!3d-6.8544!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e14a53eeae25%3A0x8f3a3c3c3c3c3c3c!2sCigugur%20Girang%2C%20Parongpong%2C%20Bandung%20Barat%20Regency%2C%20West%20Java!5e0!3m2!1sen!2sid!4v1705000000000!5m2!1sen!2sid",
+  },
+  
+  services: [
+    {
+      title: "Penerbitan Buku",
+      description: "Layanan penerbitan buku profesional dengan standar kualitas tinggi",
+      icon: "BookOpen",
+    },
+    {
+      title: "Editing & Proofreading",
+      description: "Tim editor berpengalaman untuk menyempurnakan naskah Anda",
+      icon: "Edit",
+    },
+    {
+      title: "Desain Cover",
+      description: "Desain cover menarik yang mencerminkan isi buku Anda",
+      icon: "Palette",
+    },
+    {
+      title: "ISBN & Distribusi",
+      description: "Pengurusan ISBN dan distribusi ke seluruh Indonesia",
+      icon: "Truck",
+    },
+  ],
+  
+  stats: [
+    { value: "500+", label: "Buku Diterbitkan" },
+    { value: "1000+", label: "Penulis Bergabung" },
+    { value: "15+", label: "Tahun Pengalaman" },
+    { value: "50+", label: "Penghargaan" },
+  ],
+};
+
+export const NAV_LINKS = [
+  { href: "/", label: "Beranda" },
+  { href: "/about", label: "Tentang Kami" },
+  { href: "/books", label: "Katalog Buku" },
+  { href: "/services", label: "Layanan" },
+  { href: "/events", label: "Events" },
+  { href: "/contact", label: "Kontak" },
+];
